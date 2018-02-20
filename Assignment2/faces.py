7ceff4c04b58daa5325bf81a197a4e0d2ca12a6d
wongmat <matthewck.wong@mail.utoronto.ca>
2018-02-20 18:46:25 -0500

    Added proper shuffling to get 20 images of each actor/actress for test set.

--- Assignment2/faces.py
+++ Assignment2/faces.py
@@ -131,13 +131,58 @@ def getDataLabels(actors):
 
 def shuffle(data, labels, percentage):
     np.random.seed(1)
+
+    # Getting the test data
     complete = np.vstack((data, labels))
+    bracco = int(np.sum(complete[3072, :], axis=0))
+    gilpin = int(np.sum(complete[3073, :], axis=0))
+    harmon = int(np.sum(complete[3074, :], axis=0))
+    baldwin = int(np.sum(complete[3075, :], axis=0))
+    hader = int(np.sum(complete[3076, :], axis=0))
+    carell = int(np.sum(complete[3077, :], axis=0))
+
+    # creating actor-specific subarrays and extracting the first twenty examples
+    bracco_matrix = complete[:, :bracco]
+    gilpin_matrix = complete[:, bracco:bracco+gilpin]
+    harmon_matrix = complete[:, bracco+gilpin:bracco+gilpin+harmon]
+    baldwin_matrix = complete[:, bracco+gilpin+harmon:bracco+gilpin+harmon+baldwin]
+    hader_matrix = complete[:, bracco+gilpin+harmon+baldwin:bracco+gilpin+harmon+baldwin+hader]
+    carell_matrix = complete[:, bracco+gilpin+harmon+baldwin+hader:bracco+gilpin+harmon+baldwin+hader+carell]
+    test_bracco = bracco_matrix[:, :20]
+    test_gilpin = gilpin_matrix[:, :20]
+    test_harmon = harmon_matrix[:, :20]
+    test_baldwin = baldwin_matrix[:, :20]
+    test_hader = hader_matrix[:, :20]
+    test_carell = carell_matrix[:, :20]
+    testData = np.hstack((test_bracco, test_gilpin))
+    testData = np.hstack((testData, test_harmon))
+    testData = np.hstack((testData, test_baldwin))
+    testData = np.hstack((testData, test_hader))
+    testData = np.hstack((testData, test_carell))
+    testLabels = testData[3072:, :]
+    testData = testData[:-6, :]
+
+
+    # Need to do something similar for the remaining dataset
+
+    bracco_matrix = bracco_matrix[:, 20:]
+    gilpin_matrix = gilpin_matrix[:, 20:]
+    harmon_matrix = harmon_matrix[:, 20:]
+    baldwin_matrix = baldwin_matrix[:, 20:]
+    hader_matrix = hader_matrix[:, 20:]
+    carell_matrix = carell_matrix[:, 20:]
+    complete = np.hstack((bracco_matrix, gilpin_matrix))
+    complete = np.hstack((complete, harmon_matrix))
+    complete = np.hstack((complete, baldwin_matrix))
+    complete = np.hstack((complete, hader_matrix))
+    complete = np.hstack((complete, carell_matrix))
     complete = np.transpose(complete)
     np.random.shuffle(complete)
     complete = np.transpose(complete)
     trainData, validData = complete[:-6, :int(percentage*complete.shape[1])], complete[:-6, int(percentage*complete.shape[1]):int(complete.shape[1])]
     trainLabels, validLabels = complete[3072:, :int(percentage*complete.shape[1])], complete[3072:, int(percentage*complete.shape[1]):int(complete.shape[1])]
-    return trainData, validData, trainLabels, validLabels
+
+    return trainData, validData, testData, trainLabels, validLabels, testLabels
 
 
 # Second main function
@@ -151,40 +196,66 @@ def main():
     getPictures()
     X = getDataMatrix(act)
     Y = getDataLabels(act)
-    trainData, validData, trainLabels, validLabels = shuffle(X, Y, 0.8)
+    trainData, validData, testData, trainLabels, validLabels, testLabels = shuffle(X, Y, 0.7)
     trainData = np.transpose(trainData)
     trainLabels = np.transpose(trainLabels)
     validData = np.transpose(validData)
     validLabels = np.transpose(validLabels)
+    testData = np.transpose(testData)
+    testLabels = np.transpose(testLabels)
+
     print(trainData.shape)
     print(validData.shape)
     print(trainLabels.shape)
     print(validLabels.shape)
+    print(testData.shape)
+    print(testLabels.shape)
+
+    print("Number of Bracco in training set: " + str(int(np.sum(trainLabels[:, 0], axis=0))))
+    print("Number of Gilpin in training set: " + str(int(np.sum(trainLabels[:, 1], axis=0))))
+    print("Number of Harmon in training set: " + str(int(np.sum(trainLabels[:, 2], axis=0))))
+    print("Number of Baldwin in training set: " + str(int(np.sum(trainLabels[:, 3], axis=0))))
+    print("Number of Hader in training set: " + str(int(np.sum(trainLabels[:, 4], axis=0))))
+    print("Number of Carell in training set: " + str(int(np.sum(trainLabels[:, 5], axis=0))))
+
+    print("Number of Bracco in validation set: " + str(int(np.sum(validLabels[:, 0], axis=0))))
+    print("Number of Gilpin in validation set: " + str(int(np.sum(validLabels[:, 1], axis=0))))
+    print("Number of Harmon in validation set: " + str(int(np.sum(validLabels[:, 2], axis=0))))
+    print("Number of Baldwin in validation set: " + str(int(np.sum(validLabels[:, 3], axis=0))))
+    print("Number of Hader in validation set: " + str(int(np.sum(validLabels[:, 4], axis=0))))
+    print("Number of Carell in validation set: " + str(int(np.sum(validLabels[:, 5], axis=0))))
+
+    print("Number of Bracco in test set: " + str(int(np.sum(testLabels[:, 0], axis=0))))
+    print("Number of Gilpin in test set: " + str(int(np.sum(testLabels[:, 1], axis=0))))
+    print("Number of Harmon in test set: " + str(int(np.sum(testLabels[:, 2], axis=0))))
+    print("Number of Baldwin in test set: " + str(int(np.sum(testLabels[:, 3], axis=0))))
+    print("Number of Hader in test set: " + str(int(np.sum(testLabels[:, 4], axis=0))))
+    print("Number of Carell in test set: " + str(int(np.sum(testLabels[:, 5], axis=0))))
 
     # Setting up the pytorch model
-    dim_x = 32*32*3
-    dim_h = 20
-    dim_out = 6
-    dtype_float = torch.FloatTensor
-    dtype_long = torch.LongTensor
-    model = torch.nn.Sequential(torch.nn.Linear(dim_x, dim_h), torch.nn.ReLU(), torch.nn.Linear(dim_h, dim_out),)
-    loss_func = torch.nn.CrossEntropyLoss()
-
-    x = Variable(torch.from_numpy(trainData), requires_grad=False).type(dtype_float)
-    y_classes = Variable(torch.from_numpy(np.argmax(trainLabels, 1)), requires_grad=False).type(dtype_long)
-
-    learning_rate = 1e-3
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    for t in range(10000):
-        y_pred = model(x)
-        loss = loss_func(y_pred, y_classes)
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    x = Variable(torch.from_numpy(validData), requires_grad=False).type(dtype_float)
-    y_pred = model(x).data.numpy()
-    print(np.mean(np.argmax(y_pred, 1)==np.argmax(validLabels, 1)))
+    # dim_x = 32*32*3
+    # dim_h = 20
+    # dim_out = 6
+    # dtype_float = torch.FloatTensor
+    # dtype_long = torch.LongTensor
+    # model = torch.nn.Sequential(torch.nn.Linear(dim_x, dim_h), torch.nn.ReLU(), torch.nn.Linear(dim_h, dim_out),)
+    # loss_func = torch.nn.CrossEntropyLoss()
+    #
+    # x = Variable(torch.from_numpy(trainData), requires_grad=False).type(dtype_float)
+    # y_classes = Variable(torch.from_numpy(np.argmax(trainLabels, 1)), requires_grad=False).type(dtype_long)
+    #
+    # learning_rate = 1e-3
+    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # for t in range(10000):
+    #     y_pred = model(x)
+    #     loss = loss_func(y_pred, y_classes)
+    #     model.zero_grad()
+    #     loss.backward()
+    #     optimizer.step()
+    #
+    # x = Variable(torch.from_numpy(validData), requires_grad=False).type(dtype_float)
+    # y_pred = model(x).data.numpy()
+    # print(np.mean(np.argmax(y_pred, 1)==np.argmax(validLabels, 1)))
 
     # filename = os.path.join(os.getcwd(), "cropped/")
     # filename = filename + str("baldwin0.jpg")
